50ca2fc3a157b8ec5a4c615a4f44f4110f9de5b3
qhdgkdbs <qhdgkdbs@gmail.com>
2021-03-16 23:16:33 +0900

    edit README

--- RAW_data/news/getNews.py
+++ RAW_data/news/getNews.py
@@ -1,6 +1,14 @@
 import requests
 import pandas as pd 
 from pprint import pprint
+import json
+import os
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+NAVER_ID = config['NAVER']['ID'] # 'secret-key-of-myapp'
+NAVER_SECRET = config['NAVER']['Secret'] # 'web-hooking-url-from-ci-service'
 
 
 encode_type = 'json'
@@ -9,8 +17,8 @@ keyword = ['문재인', '이재명', '윤석열', '이낙연', '홍준표']
 # keyword = ['삼성', '엘지', 'LG', '테슬라', '애플']
 
 headers = {
-    'X-Naver-Client-Id' : 'UlYgMB3RI252lyb5TT9h',
-    'X-Naver-Client-Secret' : 'N30oS9vD5m'
+    'X-Naver-Client-Id' : NAVER_ID,
+    'X-Naver-Client-Secret' : NAVER_SECRET
 }
 
 for key in keyword:
@@ -27,6 +35,9 @@ for key in keyword:
         print(type(datas), type(datas['items']))
 
         df = pd.DataFrame(datas['items'])
-        df.to_csv('naver_{}_검색결과.csv'.format(key), encoding='utf-8', index=False)
+
+        if not os.path.exists("./data"):
+            os.makedirs("./data")
+        df.to_csv('./data/naver_{}_검색결과.csv'.format(key), encoding='utf-8', index=False)
     else:
         print(res)
